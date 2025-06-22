db19083172a213b7890289686bb52b5363c04f3e
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-06-22 13:24:33 +0530

    fs

--- database/migrations/2025_06_17_160837_create_teams_table.php
+++ database/migrations/2025_06_17_160837_create_teams_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('owner_id')->constrained('users');
             $table->timestamps();
             $table->softDeletes();
         });
