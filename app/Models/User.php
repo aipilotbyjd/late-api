1127226f3fe76a76c0143de587556cc1ce9589d1
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-06-22 16:10:07 +0530

    das

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -65,5 +66,13 @@ class User extends Authenticatable
         return $this->connectedAccounts()
             ->where('provider', $provider)
             ->first();
+    }
+
+    /**
+     * Get the teams for the user.
+     */
+    public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Team::class, 'team_user');
     }
 }
